8799b5d84cf99ff4c8c42d985e2e76333afa2a4a
Jerrod Havig <secure.havig@gmail.com>
2024-07-21 12:01:28 -0700

    fixed telescope

--- lua/cursed/keymap.lua
+++ lua/cursed/keymap.lua
@@ -45,22 +45,9 @@ vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
 -- Format code
 vim.keymap.set("n", "<C-f>", vim.lsp.buf.format, {})
 
--- [[ Configure Telescope ]]
--- See `:help telescope` and `:help telescope.setup()`
-require("telescope").setup({
-  defaults = {
-    mappings = {
-      i = {
-        ["<C-u>"] = false,
-        ["<C-d>"] = false,
-      },
-    },
-  },
-})
-
--- Enable telescope fzf native, if installed
-pcall(require("telescope").load_extension, "fzf")
-
-vim.keymap.set("n", "<C-p>", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
-vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
-vim.keymap.set("n", "<C-h>", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+-- Telescope 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "[S]earch [F]iles"})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
