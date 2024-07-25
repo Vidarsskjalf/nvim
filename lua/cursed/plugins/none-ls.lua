4c04c2f2cacab5ee123d43df9c7487f9287ab1ca
Jerrod Havig <secure.havig@gmail.com>
2024-07-24 18:38:29 -0700

    Set prettier to use single quotes

--- lua/cursed/plugins/none-ls.lua
+++ lua/cursed/plugins/none-ls.lua
@@ -8,7 +8,7 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.gofmt,
-        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.prettier.with({ extra_args = { "--single-quote", "--jsx-single-quote" } }),
         null_ls.builtins.formatting.black,
         null_ls.builtins.diagnostics.eslint_d,
       },
